c3cd117cb7df3f83b194889b01e5078544a83e76
sunnynikhil <37411671+sunnynikhil@users.noreply.github.com>
2020-10-12 14:21:05 +0530

    Update graphs.R

--- graphs.R
+++ graphs.R
@@ -2,11 +2,11 @@
 addtionsdeletionsData <- read.csv("/Users/oyo/NetBeansProjects/javacourse/src/CIQTask/additions_deletions.csv")
 
 
-barplot(c(addtionsdeletionsData$a),names.arg=c(addtionsdeletionsData$w),main="Number of additions activity",xlab="week",ylab="Number of Additions",density=15)
+barplot(c(addtionsdeletionsData$a),names.arg=c(addtionsdeletionsData$w),main="Number of additions activity",xlab="weeks",ylab="Number of Additions",density=15)
 
-barplot(c(mydata$d),names.arg=c(mydata$w),main="Number of Deletion activity",xlab="week",ylab="Number of Deletions")
+barplot(c(addtionsdeletionsData$d),names.arg=c(addtionsdeletionsData$w),main="Number of Deletion activity",xlab="weeks",ylab="Number of Deletions")
 
 #Read last year commit csv data
 yearlyCommittedData <- read.csv("/Users/oyo/NetBeansProjects/javacourse/src/CIQTask/last_year_commit_activity.csv")
 
-barplot(c(yearlyCommittedData$total),names.arg=c(yearlyCommittedData$week),main="Last Year Commit Activity ",xlab="week",ylab="Number of Commits",density=15)
+barplot(c(yearlyCommittedData$total),names.arg=c(yearlyCommittedData$week),main="Last Year Commit Activity ",xlab="weeks",ylab="Number of Commits",density=15)
